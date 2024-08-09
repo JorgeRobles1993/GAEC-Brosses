aa3ff9e5a1328b18f8f07de7d8c48fde194c23e5
JorgeRobles1993 <jorgejoanrobles93@gmail.com>
2024-08-09 16:16:58 +0200

    tailwind home test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vert: {
+          DEFAULT: '#4CAF50',
+          fonce: '#388E3C',
+          clair: '#C8E6C9',
+        },
+        marron: {
+          DEFAULT: '#8D6E63',
+          fonce: '#6D4C41',
+          clair: '#D7CCC8',
+        },
+        beige: {
+          DEFAULT: '#F5F5DC',
+          fonce: '#E0E0D1',
+          clair: '#FFFBEA',
+        },
+      },
+    },
   },
   plugins: [],
 };
