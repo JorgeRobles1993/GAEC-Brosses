4802c7e6181b10038914bf5755de8f6df2eadaca
JorgeRobles1993 <jorgejoanrobles93@gmail.com>
2024-09-02 16:49:48 +0200

    routers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { HomePageComponent } from './app/home-page/home-page.component';
+import { VisitComponent } from './app/visit/visit.component';  // Actualizado
+import { AProposComponent } from './app/a-propos/a-propos.component';
+import { ContactComponent } from './app/contact/contact.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      [
+        { path: '', component: HomePageComponent },
+        { path: 'visit', component: VisitComponent },  // Ruta actualizada
+        { path: 'a-propos', component: AProposComponent },
+        { path: 'contact', component: ContactComponent },
+      ],
+      withComponentInputBinding()
+    ),
+  ],
+}).catch((err) => console.error(err));
