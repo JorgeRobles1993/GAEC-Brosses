116be1931249cb5569c683c45f5323d8db24cc21
JorgeRobles1993 <jorgejoanrobles93@gmail.com>
2024-09-03 16:07:34 +0200

    carrousel a medio terminar

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CarouselComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
